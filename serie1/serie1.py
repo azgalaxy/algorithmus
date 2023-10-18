e05bbe392e57efdd6dfb183b905a3f7120a4ac1b
bzardi478 <87087954+bzardi478@users.noreply.github.com>
2023-10-18 22:31:04 +0200

    Update serie1.py

--- serie1/serie1.py
+++ serie1/serie1.py
@@ -1,82 +1,72 @@
-#EXERCICE 1
-def Nombre_negatif_ou_postif_v1():
-    num = int(input("Entrez Un nombre : "))
+# EXERCICE 1
+def EXERCICE1():
+    num = int(input("Entrer Un nombre alétoire: "))
 
     if num > 0:
-        print("Le nombre est négatif")
+        print("Cette nombre est postif")
+    else:
+        if num < 0:
+            print(("Cette nombre est négatif"))
+
+
+# EXERCICE 2
+def EXERCICE2():
+    num1 = int(input("Entrez un nombre: "))
+    num2 = int(input("Entrez un nombre: "))
+
+    if (num1 > 0 and num2 > 0) or (num1 < 0 and num2 < 0):
+        print("Le produit est positif")
+    else:
+        if (num1 < 0 and num2 > 0) or (num1 > 0 and num2 < 0):
+            print("Le produit est négatif")
+
+
+# EXERCICE 3
+def EXERCICE3():
+    num = int(input("Entrer Un nombre alétoire:"))
+
+    if num == 0:
+        raise ValueError("Le nombre 0 n'est ni négatif ni positif")
     elif num < 0:
-        print("Le nombre est positif")
+        print("Cette nombre est négatif")
+    elif num > 0:
+        print("Cette nombre est positif")
 
-#EXERCICE 2
-def Produit_negatif_ou_positif_v1():
-    num1 = int(input("Entrer Un nombre: "))
-    num2 = int(input("Entrer Un autre nombre:"))
 
-	if (num1 > 0 ET num2 > 0) or (num1 < 0 ET num2 < 0):
-		print("Le produit est postif")
-	elif (num1 < 0 ET num2 > 0) or (num1 > 0 ET num2 < 0):
-			Ecrire("Le produit est négatif")
+# EXERCICE 4
 
-#EXERCICE 3
-def Nombre_negatif_ou_positif_v2():
-    num = int(input("Entrer Un nombre:"))
 
-	if num == 0:
-		print("Le nombre 0 n'est ni négatif ni positif")
-	elif num < 0:
-        print("Le nombre est négatif")
-    else    
-        print("Le nombre est positif")
-	FinSi   
+def EXERCICE4():
+    num1 = int(input("Entrez un nombre: "))
+    num2 = int(input("Entrez un autre nombre: "))
 
-#EXERCICE 4
-
-def Produit_negatif_ou_positif_v2():
-    num1 = int(input("Entrer Un nombre: "))
-    num2 = int(input("Entrer Un autre nombre: "))
-
-    if (num1 <0 ):
-        ps1 = False
+    if num1 == 0 or num2 == 0:
+        if num1 == 0 or num2 == 0:
+            print("Le produit est 0")
     else:
-        ps1 = True
-
-    if (num2 < 0):
-        ps2 = False
-    else:
-        ps2 = True
-
-    if (num1 == 0 ):
-        raise ValueError("0 est incorrectible")
-    elif (num2 ==0 ):
-        raise ValueError("0 est incorrectible")
-    elif ps1 == True and ps2 == True:
-        print("Le nombre est positif")
-    elif ps1 == True and ps2 == False:
-        print("Le nombre est négatif")
-    elif ps2 == True and ps1 == False:
-        print("Le nombre est négatif")
-    elif ps2 == False and ps1 == False:
-        print("Le nombre est positif")
+        if (num1 > 0 and num2 > 0) or (num1 < 0 and num2 < 0):
+            print("Le produit est positif")
+        else:
+            print("Le produit est négatif")
 
 
-#EXERCICE 5
+# EXERCICE 5
 def EXERCICE5():
-    age = int(input("Entrer Ton Age: "))
+    age = int(input("Entez l'age d'un enfant: "))
 
-    if age in [6,7]:
-        print("Poussin")
+    if age >= 6 and age <= 7:
+        print("La catégorie de l'enfant est Poussin")
+    else:
+        if age >= 8 and age <= 9:
+            print("La catégorie de l'enfant est Pupille")
+        else:
+            if age >= 10 and age <= 11:
+                print("La catégorie de l'enfant est Pupille")
+            else:
+                print("La catégorie de l'enfant est Cadet")
 
-    if age in [8,9]:
-        print("Pupille")
-    if age in [10,11]:
-        print("Minime")
 
-    if age in [12,]:
-        print("Cadet")
-
-    if age == 0:
-       exit()
-
+EXERCICE5()
 
 
 
